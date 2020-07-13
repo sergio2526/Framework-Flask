9c8b439ce4d31550c947f2586bcc6e13eb76583f
sergio2526 <sergiorubiano25@hotmail.com>
2020-07-12 21:19:05 -0500

    Agregando cambios de prueba

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, request, Response, make_response, redirect , render_template, abort, session
+from flask import Flask, request, Response, make_response, \
+redirect , render_template, abort, session, url_for, flash
 
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField , SubmitField
+from wtforms.validators import DataRequired
+import unittest
 
 
 app = Flask(__name__)
@@ -8,7 +13,20 @@ app.config['ENV']='development'
 app.config['SECRET_KEY'] = 'SUPER SECRETO' # gererar sessión en flask
 bootstrap = Bootstrap(app)
 
-todos = ['Leer2', 'Platzi', 'Actividades2','GCP']
+todos = ['Leer2', 'Platzi', 'Actividades','GCP']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('ENviar')
+
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -26,13 +44,26 @@ def index():
     session['user_ip'] = user_ip
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET','POST'])
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
     context = {
         'user_ip':user_ip,
-        'todos':todos
+        'todos':todos,
+        'login_form':login_form,
+        'username':username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usuario registrado con éxito.')
+
+        return redirect(url_for('index'))
+
     return render_template('hello.html', **context) #Expandir diccionario con ** es lo mismo que una instancia de objeto
 
 if __name__ == '__main__':
